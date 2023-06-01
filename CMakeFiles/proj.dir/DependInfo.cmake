ed979703c11c76d046eef6e32fe99edae64abfde
mustafaakbaba <mustafa093158@gmail.com>
2023-06-01 20:34:11 +0300

    adding files

---
+++ CMakeFiles/proj.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/betul/ME413/dealii/project/project_part1/proj.cc" "/home/betul/ME413/dealii/project/project_part1/CMakeFiles/proj.dir/proj.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/betul/ME413/dealii/include"
+  "/home/betul/ME413/dealii/bundled/umfpack/UMFPACK/Include"
+  "/home/betul/ME413/dealii/bundled/umfpack/AMD/Include"
+  "/home/betul/ME413/dealii/bundled/kokkos-3.7.00/algorithms/src"
+  "/home/betul/ME413/dealii/bundled/kokkos-3.7.00/containers/src"
+  "/home/betul/ME413/dealii/bundled/kokkos-3.7.00/core/src"
+  "/home/betul/ME413/dealii/bundled/kokkos-3.7.00/simd/src"
+  "/home/betul/ME413/dealii/bundled/kokkos-3.7.00/tpls/desul/include"
+  "/home/betul/ME413/dealii/bundled/muparser_v2_3_3/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
